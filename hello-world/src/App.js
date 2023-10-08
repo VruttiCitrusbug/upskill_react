e29aad0eb642a67e4ddf89583ffe14d044e91219
vrutti.citrusbug@gmail.com <vrutti.citrusbug@gmail.com>
2023-10-08 12:01:25 +0530

    10

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -27,7 +27,7 @@ class App extends Component{
   render(){
     return(
       <div className="App">
-        {/* <h1 className='error'>ERRROR</h1>
+        {/* <h1 className='error'>ERROR</h1>
         <h1 className={styles.success}>SUCCESS</h1> */}
       {/* <Welcome /> */}
       {/* <Hello name="vrutti" surname="patel">
