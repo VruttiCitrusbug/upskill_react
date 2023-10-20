1ec4a17a7191c93ed3535f11e8e09ba9dbbd4f49
Vrutti Patel <vrutti.citrusbug@gmail.com>
2023-10-20 17:54:17 +0530

    20-10-2023

---
+++ react-params/src/user.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        User 1
+        User 2
+        User 3
+    </div>
+  );
+}
+
+export default App;
---
+++ react-params/src/userdetail.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+
+function UserDetail() {
+  return (
+    <><div>UserDetail</div><Outlet /></>
+  )
+}
+
+export default UserDetail
